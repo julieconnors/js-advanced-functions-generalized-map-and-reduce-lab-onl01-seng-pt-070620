51566c17232d775545ad2241a6aaab863f01bda8
julieconnors <juliana.g.connors@gmail.com>
2021-01-31 06:26:49 -0500

    commit tree 51e863a6a6fbd1440b872f6d8b22478d56323a5b

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Add your functions here
+function map(source, callBack) {
+    let newList = [];
+    for (let i = 0; i < source.length; i++) {
+
+        newList.push(callBack(source[i]));
+    }
+    return newList;
+}
+
+function reduce(source, callBack, start) {
+    let result = start;
+    for (let i = 0; i < source.length; i++) {
+         result = callBack(source[i]);
+    }
+
+    return result;
+}
